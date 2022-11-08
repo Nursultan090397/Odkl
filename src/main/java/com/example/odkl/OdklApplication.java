b4d113b50c6cadedc80d1571f051abaddf020fdc
osorov97 <osorov.nursultan97@gmail.com>
2022-11-08 15:48:22 +0600

    Initial commit

---
+++ src/main/java/com/example/odkl/OdklApplication.java
@@ -0,0 +1,13 @@
+package com.example.odkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdklApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdklApplication.class, args);
+	}
+
+}
