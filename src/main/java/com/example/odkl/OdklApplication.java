dcf5028abc21ecb939c74ba2f68cc4ec3a973542
osorov97 <osorov.nursultan97@gmail.com>
2022-11-08 15:48:41 +0600

    odkl

--- src/main/java/com/example/odkl/OdklApplication.java
+++ src/main/java/com/example/odkl/OdklApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OdklApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OdklApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OdklApplication.class, args);
+    }
 
 }
